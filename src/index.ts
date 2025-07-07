4983afe67f03954dec517e0f8563c886590364bc
yangxu <yangxu01@changba.com>
2025-07-07 17:47:09 +0800

    feat: 🎸 增加组件销毁处理

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,23 @@ export { player };
 export default {
   install(Vue: VueConstructor) {
     Vue.prototype.$player = player;
+
     const MusicMixin = {
       data() {
+        const __stateChangeCallback = (state: string) => {
+          // @ts-ignore
+          this.cbMusicPlugin.playState = state === "playing";
+        };
+
+        const __musicChangeCallback = (music: string) => {
+          // @ts-ignore
+          this.cbMusicPlugin.current = music;
+        };
         return {
+          callback: {
+            __stateChangeCallback,
+            __musicChangeCallback,
+          },
           cbMusicPlugin: {
             playState: "stop",
             current: {},
@@ -18,24 +32,35 @@ export default {
         // @ts-ignore
         this.listenMusicChange();
       },
+      beforeDestroy() {
+        // @ts-ignore
+        this.removeListenMusicChange();
+      },
       methods: {
+        removeListenMusicChange() {
+          // @ts-ignore
+          if (!this.$options.shouldListenPlayerChange) {
+            return;
+          }
+          // @ts-ignore
+          player.removeListener("state-change", this.callback.__stateChangeCallback);
+          // @ts-ignore
+          player.removeListener("music-change", this.callback.__musicChangeCallback);
+        },
         listenMusicChange() {
           // 为了考虑性能，不需要对所有的组件都施加监听
           // @ts-ignore
           if (!this.$options.shouldListenPlayerChange) {
             return;
           }
-          player.on("state-change", (state) => {
-            // @ts-ignore
-            this.cbMusicPlugin.playState = state === "playing";
-          });
-          player.on("music-change", (music) => {
-            // @ts-ignore
-            this.cbMusicPlugin.current = music;
-          });
+          // @ts-ignore
+          player.on("state-change", this.callback.__stateChangeCallback);
+          // @ts-ignore
+          player.on("music-change", this.callback.__musicChangeCallback);
         },
       },
     };
+
     Vue.mixin(MusicMixin);
   },
 };
